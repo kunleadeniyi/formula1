48020b9b0c08c413868fee0a8b3a594fd7f193ac
Kunle Adeniyi <adeniyikunle22@gmail.com>
2023-08-03 22:43:06 +0000

    added overall team stats

--- dbt/models/stats/overall/driver/overall_driver_podiums.sql
+++ dbt/models/stats/overall/driver/overall_driver_podiums.sql
@@ -1,13 +1,21 @@
 with podiums as (
     select driver_id, constructor_id from {{ ref('stg_race_results') }}
     where position in (1,2,3)
+),
+drivers as (
+    select
+        driver_id,
+        given_name || ' ' || family_name as driver,
+        nationality
+    from {{ ref('stg_drivers') }}
 )
 select 
     rank() over (order by count(a.driver_id) desc)::varchar as pos,
-    b.given_name || ' ' || b.family_name as driver, 
+    b.driver, 
+    b.nationality as nat,
     count(a.driver_id)::varchar as podiums
 from podiums a, 
-    {{ ref('stg_drivers') }} b
+    drivers b
 where a.driver_id = b.driver_id
-group by b.given_name || ' ' || b.family_name
+group by b.driver, b.nationality
 order by count(a.driver_id) desc
--- dbt/models/stats/overall/driver/overall_driver_race_wins.sql
+++ dbt/models/stats/overall/driver/overall_driver_race_wins.sql
@@ -1,13 +1,21 @@
 with winners as (
     select driver_id, constructor_id from {{ ref('stg_race_results') }}
     where position = 1
+),
+drivers as (
+    select
+        driver_id,
+        given_name || ' ' || family_name as driver,
+        nationality
+    from {{ ref('stg_drivers') }}
 )
 select 
     rank() over (order by count(a.driver_id) desc)::varchar as pos,
-    b.given_name || ' ' || b.family_name as driver, 
+    b.driver, 
+    b.nationality as nat,
     count(a.driver_id)::varchar as wins
 from winners a, 
-    {{ ref('stg_drivers') }} b
+    drivers b
 where a.driver_id = b.driver_id
-group by b.given_name || ' ' || b.family_name
+group by b.driver, b.nationality
 order by count(a.driver_id) DESC
